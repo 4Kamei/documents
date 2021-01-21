4cccf545a3174512fe2a68ba95ad393d63a618e6
4Kamei <theinfamouspig@gmail.com>
2021-01-21 17:29:11 +0000

    finished random walk code

--- networks_1/random_walk.py
+++ networks_1/random_walk.py
@@ -2,6 +2,8 @@ import random as r
 from functools import reduce
 import numpy as np
 
+SAMPLE_COUNT = 10000
+
 def iterate(sample):
     for i in range(len(sample)):
         sample[i] += -1 if r.random() < 0.5 else 1
@@ -9,7 +11,24 @@ def iterate(sample):
 
 def get_data(sample):
     (vals, freq) = np.unique(sample, return_counts=True)
+    freq = list(map(lambda x: x/SAMPLE_COUNT, freq))
+    return (vals, freq)
 
-#start with 100 values
-sample = [0 for i in range(100)]
+#start with 1000 values
+sample = [0 for i in range(SAMPLE_COUNT)]
 
+max_iter = 1000
+
+for i in range(max_iter):
+    #iterate the random walk
+    sample = iterate(sample)
+
+#save the final sample
+
+vals, freq = get_data(sample)
+
+f = open("random_walk.csv", "w")
+f.write("pos, prob")
+for j in range(len(freq)):
+    f.write("\n{}, {}".format(vals[j], freq[j]))
+f.close()
